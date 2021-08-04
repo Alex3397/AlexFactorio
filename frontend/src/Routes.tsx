8ff4bf9273da0da0980cb211e276ccf48966b452
Alexandre <xandhy3369@gmail.com>
2021-08-04 15:19:36 -0300

    forcing existence

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -9,10 +9,45 @@ function Routes() {
         <BrowserRouter>
             <Switch>
                 <Route path="/login">
+                    <h1>LOGIN</h1>
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <p>
+                                Edit <code>src/App.tsx</code> and save to reload.
+                            </p>
+                            <a
+                                className="App-link"
+                                href="/login"
+                            >
+                                Sing In
+                            </a>
+                            <a className="App-link" href="/BankApp">
+                                BankApp
+                            </a>
+                        </header>
+                    </div>
                     <div style={{ color: 'white' }}>Alguma coisa</div>
                 </Route>
                 <Route path="/BankApp">
                     <div style={{ color: 'white' }}>Outra coisa</div>
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <p>
+                                Edit <code>src/App.tsx</code> and save to reload.
+                            </p>
+                            <a
+                                className="App-link"
+                                href="/login"
+                            >
+                                Sing In
+                            </a>
+                            <a className="App-link" href="/BankApp">
+                                BankApp
+                            </a>
+                        </header>
+                    </div>
                     <BankApp/>
                 </Route>
                 <Route path="/">
